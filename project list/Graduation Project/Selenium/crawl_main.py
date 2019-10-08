74c226f9535fa60cd4433444205b65f2aac5daa6
JMH <battlesun99@gamil.com>
2019-10-08 21:06:45 +0900

    commit

--- project list/Graduation Project/Selenium/crawl_main.py
+++ project list/Graduation Project/Selenium/crawl_main.py
@@ -15,81 +15,172 @@ import crawl_parseScreenshot
 import crawl_saveMongoDB
 import crawl_saveExcel
 from datetime import datetime
+from multiprocessing import Pool # 빠른 처리속도를 위해 멀티 프로세싱
+import threading # 빠른 처리속도를 위해 멀티 스레딩
+import time
 
-def main(jsonFile):
-    # 검색 페이지인 경우를 알기 위한 변수
-    search_page = 'https://www.google.com/search?'
+class Threading(threading.Thread):
+    def __init__(self, jsonFile):
+        # 검색 페이지인 경우를 알기 위한 변수
+        self.search_page = 'https://www.google.com/search?'
 
-    # 메인 함수에 들어오는 json 객체 파일 열기
-    with open(jsonFile, encoding='utf-8') as f:
-        data = json.loads(f.read())
+        # 메인 함수에 들어오는 json 객체 파일 열기
+        with open(jsonFile, encoding='utf-8') as f:
+            data = json.loads(f.read())
 
-        user_name = data['user_name']
-        user_email = data['user_email']
-        curr_url = data['curr_url']
-        prev_url = data['prev_url']
-        paths = data['paths']
-        level = data['level']
-        tagged = data['tagged']
-        memo = data['memo']
-        project_name = data['project_name']
+            self.user_name = data['user_name']
+            self.user_email = data['user_email']
+            self.curr_url = data['curr_url']
+            self.prev_url = data['prev_url']
+            self.paths = data['paths']
+            self.level = data['level']
+            self.tagged = data['tagged']
+            self.memo = data['memo']
+            self.project_name = data['project_name']
 
-    # 현재 페이지에 대한 크롤링 준비
-    driver = crawl_initiateChromeDriver.initiateChromeDriver()
-    driver.get(curr_url)
-    driver.implicitly_wait(2)
+        # 현재 페이지에 대한 크롤링 준비
+        self.driver = crawl_initiateChromeDriver.initiateChromeDriver()
+        self.driver.get(self.curr_url)
+        self.driver.implicitly_wait(2)
 
-    # 검색 페이지인 경우
-    if search_page in curr_url:
-        print('검색 페이지인 경우')
-        user_name = user_name
-        user_email = user_email
-        keyword = crawl_parseKeyword.parseKeywordSearchPage(driver)
-        curr_url = curr_url
-        prev_url = prev_url
-        pageList = crawl_parsePageList.parsePageList(driver)
-        relativeKeywordList = crawl_parseRelativeKeyword.parseRelativeKeyword(driver)
-        pageContents = []
-        sub_keyword = []
-        tagged = tagged
-        # screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
-        nowTime = datetime.now()
+    def run(self):
+        # 검색 페이지인 경우
+        if self.search_page in self.curr_url:
+            print('검색 페이지인 경우')
+            user_name = self.user_name
+            user_email = self.user_email
+            keyword = crawl_parseKeyword.parseKeywordSearchPage(self.driver)
+            curr_url = self.curr_url
+            prev_url = self.prev_url
+            pageList = crawl_parsePageList.parsePageList(self.driver)
+            relativeKeywordList = crawl_parseRelativeKeyword.parseRelativeKeyword(self.driver)
+            pageContents = []
+            sub_keyword = []
+            tagged = self.tagged
+            # screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
+            nowTime = datetime.now()
 
-    # 홈페이지인 경우
-    else:
-        print('홈페이지인 경우')
-        user_name = user_name
-        user_email = user_email
-        keyword = crawl_parseKeyword.parseKeywordHomePage(driver)
-        curr_url = curr_url
-        prev_url = prev_url
-        pageList = []
-        relativeKeywordList = []
-
-        # 서브 키워드 및 본문 요약은 키워드가 한글 또는 영어에 따라 다르다.
-        if crawl_isEnglishOrKorean.isEnglishOrKorean(keyword) == 'kr':
-            textrank = crawl_parseKoreanContents.TextRank(curr_url)
-            pageContents = textrank.summarize(5)
-            sub_keyword = textrank.keywords()
+        # 홈페이지인 경우
         else:
-            textrank = crawl_parseEnglishContents.TextRank(curr_url)
-            pageContents = textrank.summarize(5)
-            sub_keyword = textrank.keywords()
+            print('홈페이지인 경우')
+            user_name = self.user_name
+            user_email = self.user_email
+            keyword = crawl_parseKeyword.parseKeywordHomePage(self.driver)
+            curr_url = self.curr_url
+            prev_url = self.prev_url
+            pageList = []
+            relativeKeywordList = []
 
-        tagged = tagged
-        memo = memo
-        # screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
-        nowTime = datetime.now()
+            # 서브 키워드 및 본문 요약은 키워드가 한글 또는 영어에 따라 다르다.
+            if crawl_isEnglishOrKorean.isEnglishOrKorean(keyword) == 'kr':
+                textrank = crawl_parseKoreanContents.TextRank(curr_url)
+                pageContents = textrank.summarize(5)
+                sub_keyword = textrank.keywords()
+            else:
+                textrank = crawl_parseEnglishContents.TextRank(curr_url)
+                pageContents = textrank.summarize(5)
+                sub_keyword = textrank.keywords()
 
-    # 추출된 아이템들을 몽고 DB에 저장
-    crawl_saveMongoDB.store_mongoDB(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, tagged, nowTime, project_name)
+            tagged = self.tagged
+            memo = self.memo
+            # screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
+            nowTime = datetime.now()
 
-    # # 추출된 아이템들을 엑셀에 저장
-    # crawl_saveExcel.make_excel(user_name, keyword)
-    # crawl_saveExcel.crawl_saveExcel(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, tagged, str(nowTime))
+        # 추출된 아이템들을 몽고 DB에 저장
+        crawl_saveMongoDB.store_mongoDB(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, self.level, self.paths, keyword, sub_keyword, pageContents, memo, tagged, nowTime, self.project_name)
 
-    # 드라이버 연결 끊기
-    driver.close()
+        # # 추출된 아이템들을 몽고 DB에 저장
+        # crawl_saveMongoDB.store_mongoDB(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, tagged, nowTime, project_name)
+
+
+        # # 추출된 아이템들을 엑셀에 저장
+        # crawl_saveExcel.make_excel(user_name, keyword)
+        # crawl_saveExcel.crawl_saveExcel(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, tagged, str(nowTime))
+
+        # 드라이버 연결 끊기
+        self.driver.close()
+
+# def main(jsonFile):
+#     # 검색 페이지인 경우를 알기 위한 변수
+#     search_page = 'https://www.google.com/search?'
+#
+#     # 메인 함수에 들어오는 json 객체 파일 열기
+#     with open(jsonFile, encoding='utf-8') as f:
+#         data = json.loads(f.read())
+#
+#         user_name = data['user_name']
+#         user_email = data['user_email']
+#         curr_url = data['curr_url']
+#         prev_url = data['prev_url']
+#         paths = data['paths']
+#         level = data['level']
+#         tagged = data['tagged']
+#         memo = data['memo']
+#         project_name = data['project_name']
+#
+#     # 현재 페이지에 대한 크롤링 준비
+#     driver = crawl_initiateChromeDriver.initiateChromeDriver()
+#     driver.get(curr_url)
+#     driver.implicitly_wait(2)
+#
+#     # 검색 페이지인 경우
+#     if search_page in curr_url:
+#         print('검색 페이지인 경우')
+#         user_name = user_name
+#         user_email = user_email
+#         keyword = crawl_parseKeyword.parseKeywordSearchPage(driver)
+#         curr_url = curr_url
+#         prev_url = prev_url
+#         pageList = crawl_parsePageList.parsePageList(driver)
+#         relativeKeywordList = crawl_parseRelativeKeyword.parseRelativeKeyword(driver)
+#         pageContents = []
+#         sub_keyword = []
+#         tagged = tagged
+#         # screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
+#         nowTime = datetime.now()
+#
+#     # 홈페이지인 경우
+#     else:
+#         print('홈페이지인 경우')
+#         user_name = user_name
+#         user_email = user_email
+#         keyword = crawl_parseKeyword.parseKeywordHomePage(driver)
+#         curr_url = curr_url
+#         prev_url = prev_url
+#         pageList = []
+#         relativeKeywordList = []
+#
+#         # 서브 키워드 및 본문 요약은 키워드가 한글 또는 영어에 따라 다르다.
+#         if crawl_isEnglishOrKorean.isEnglishOrKorean(keyword) == 'kr':
+#             textrank = crawl_parseKoreanContents.TextRank(curr_url)
+#             pageContents = textrank.summarize(5)
+#             sub_keyword = textrank.keywords()
+#         else:
+#             textrank = crawl_parseEnglishContents.TextRank(curr_url)
+#             pageContents = textrank.summarize(5)
+#             sub_keyword = textrank.keywords()
+#
+#         tagged = tagged
+#         memo = memo
+#         # screenshot = crawl_parseScreenshot.parseScreenshot(driver, user_name, keyword)
+#         nowTime = datetime.now()
+#
+#     # 추출된 아이템들을 몽고 DB에 저장
+#     crawl_saveMongoDB.store_mongoDB(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, tagged, nowTime, project_name)
+#
+#     # # 추출된 아이템들을 엑셀에 저장
+#     # crawl_saveExcel.make_excel(user_name, keyword)
+#     # crawl_saveExcel.crawl_saveExcel(user_name, user_email, curr_url, prev_url, pageList, relativeKeywordList, level, paths, keyword, sub_keyword, pageContents, memo, tagged, str(nowTime))
+#
+#     # 드라이버 연결 끊기
+#     driver.close()
 
 if __name__ == '__main__':
-    main('C:/Users/battl/PycharmProjects/ComputerScienceEngineering/2019/Graduation Project/Selenium/json/5_11.json')
+    start_time = time.time()
+
+    # main('C:/Users/battl/PycharmProjects/ComputerScienceEngineering/project list/Graduation Project/Selenium/json/6_1.json')
+
+    thread = Threading('C:/Users/battl/PycharmProjects/ComputerScienceEngineering/project list/Graduation Project/Selenium/json/6_1.json') # thread 생성
+    thread.run()
+
+    print("--- %s seconds ---" % (time.time() - start_time))
