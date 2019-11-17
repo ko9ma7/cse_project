9de4e380f7b90b670e9f76a87b66cb0ca5f6639a
JMH <battlesun99@gamil.com>
2019-11-18 06:57:47 +0900

    commit

--- activity list/Python Programming/Lab2/2-2_2014136124.py
+++
@@ -1,7 +0,0 @@
-"""문제2번"""
-"""기하학: 정다각형의 넓이"""
-import math
-n = eval(input("변의 개수를 입력하세요:"))
-s = eval(input("변의 길이를 입력하세요:"))
-Area = (n * s * s) / (4 * math.tan(math.pi / n))
-print("다각형의 넓이는",Area,"입니다.")
--- coding practice/BOJ/1001.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void) {
-	int A, B;
-	cin >> A >> B;
-	printf("%d", A - B);
-	return 0;
-}
