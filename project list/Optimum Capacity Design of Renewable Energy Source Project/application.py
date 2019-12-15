19b4c7e272c70ab8789f902715732eab4d19c799
JMH <battlesun99@gamil.com>
2019-12-15 21:20:12 +0900

    commit

---
+++ project list/Optimum Capacity Design of Renewable Energy Source Project/application.py
@@ -0,0 +1,46 @@
+import json
+import os
+import pprint
+
+from flask import Flask, render_template, session, request
+from flask_restful import Api
+import logging
+import requests
+
+from rest_client.example import example_blueprint
+from rest_client.execute import execute_blueprint
+from rest_server.rest_energy import EnergyDayResource, EnergyMonthResource
+
+application = Flask(__name__)
+application.debug = True
+application.config['SECRET_KEY'] = '12345678'
+application.register_blueprint(execute_blueprint, url_prefix='/execute')
+application.register_blueprint(example_blueprint, url_prefix='/example')
+
+api = Api(application)
+api.add_resource(EnergyDayResource, "/day-energy/<date>/area/<area>")
+api.add_resource(EnergyMonthResource, "/month-energy/<date>/area/<area>")
+
+
+@application.route('/manual')
+def manual():
+    return render_template(
+        'manual.html',
+        menu='manual'
+    )
+
+
+@application.route('/')
+def hello_html():
+    return render_template(
+        'index.html',
+        menu='main',
+    )
+
+
+if __name__ == "__main__":
+    logging.info("Flask Web Server Started!!!")
+
+    application.debug = True
+    application.config['DEBUG'] = True
+    application.run(host="0.0.0.0", port="8080")
