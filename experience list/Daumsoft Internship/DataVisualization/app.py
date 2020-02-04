519ae9233b42242c52c7b18c0652d3e9a1fa09be
JiMyungHwa <40465192+JiMyungHwa@users.noreply.github.com>
2020-02-04 19:19:32 +0900

    Add files via upload
    commit

--- experience list/Daumsoft Internship/DataVisualization/app.py
+++ experience list/Daumsoft Internship/DataVisualization/app.py
@@ -1,12 +1,18 @@
+import os
+import logging
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from dimension_reduction import dimension_reduction
-from embedding import embedding
-from machine_learning import machine_learning
-import pandas as pd
-import os
 
-UPLOAD_FOLDER = 'C:/Users/battl/PycharmProjects/ComputerScienceEngineering/experience list/Daumsoft Internship/DataVisualization/uploads'
+from embedding import embedding
+import machine_learning
+from dimension_reduction import dimension_reduction
+
+import pandas as pd
+
+from sklearn.metrics import accuracy_score
+
+
+UPLOAD_FOLDER = 'C:/Users/daumsoft/PycharmProjects/visualization/uploads'
 ALLOWED_EXTENSIONS = set(['csv'])
 
 app = Flask(__name__)
@@ -27,32 +33,101 @@ def main():
         if train_file and allowed_file(train_file.filename):
             filename1 = secure_filename(train_file.filename)
             train_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
-            return render_template('/index.html', filename1=filename1)
+            return render_template('index.html', filename1=filename1)
 
         if test_file and allowed_file(test_file.filename):
             filename2 = secure_filename(test_file.filename)
             test_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
-            return render_template('/index.html', filename2=filename2)
+            return render_template('index.html', filename2=filename2)
 
         checked_list = request.form.getlist('checked_list')
         print(checked_list)
 
-        # train = pd.read_csv("C:/Users/battl/PycharmProjects/ComputerScienceEngineering/experience list/Daumsoft Internship/DataVisualization/uploads/train.csv")
-        # test = pd.read_csv("C:/Users/battl/PycharmProjects/ComputerScienceEngineering/experience list/Daumsoft Internship/DataVisualization/uploads/test.csv")
-        #
-        # print(train)
-        #
-        # # 첫 번째 순서: 임베딩
-        # embedding_train_data = embedding(checked_list[0], train)
-        # embedding_test_data = embedding(checked_list[0], test)
-        #
-        # # 두 번째 순서: 기계학습
-        # machine_learning_train_data = machine_learning(checked_list[1], embedding_train_data)
-        # machine_learning_test_data = machine_learning(checked_list[1], embedding_test_data)
-        #
-        # # 세 번째 순서: 차원축소
-        # dimension_reduction_train_data = dimension_reduction(checked_list[2], machine_learning_train_data)
-        # dimension_reduction_test_data = dimension_reduction(checked_list[2], machine_learning_test_data)
+        # 시각화 버튼을 눌렀을 경우
+        if request.form.get("visual_button"):
+
+            train = pd.read_csv("C:/Users/daumsoft/PycharmProjects/visualization/uploads/train.csv", encoding='CP949')
+            test = pd.read_csv("C:/Users/daumsoft/PycharmProjects/visualization/uploads/test.csv", encoding='CP949')
+
+            print(train)
+            print(test)
+
+            # 첫 번째 순서: 임베딩
+            X_train, X_test, y_train, y_test = embedding(checked_list[0], train, test)
+
+            print('X_train shape: {}, y_train shape: {}'.format(X_train.shape, y_train.shape))
+            print('X_test shape: {}, y_test shape: {}'.format(X_test.shape, y_test.shape))
+
+            logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+
+            # 두 번째 순서: 기계학습
+            if checked_list[1] == 'Logistic':
+
+                log_params = {
+                    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+                    "eta0": [0.01, 0.001],
+                    'random_state': [42],
+                }
+                log_clf = machine_learning.myLogisticRegression(log_params)
+
+            elif checked_list[1] == 'SVC':
+
+                svc_params = {
+                    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+                    "gamma": [0.01, 0.1, 1],  # 커널 폭의 역수, 작을 수록 커널 폭이 넓어져, 데이터 영향 범위 증가
+                    "probability": [True],  # SVC가 'predict_proba'를 계산하기 위해 지정
+                    'random_state': [42],
+                }
+                svc_clf = machine_learning.mySVC(svc_params)
+
+                svc_clf.fit(X_train, y_train)
+
+                # 평가
+                train_y_pred = svc_clf.predict(X_test)
+                train_acc = accuracy_score(y_test, train_y_pred)
+                test_y_pred = svc_clf.predict(X_test)
+                test_acc = accuracy_score(y_test, test_y_pred)
+
+                print("svc train acc : {:.4f}, test acc :{:.4f}".format(train_acc, test_acc))
+
+            elif checked_list[1] == 'RandomForest':
+
+                rnd_params = {
+                    "n_estimators": [100],  # [100, 300, 500, 700],
+                    "max_depth": [3],  # [i for i in range(1, 31)],
+                    'random_state': [42],
+                }
+                rnd_clf = machine_learning.myRandomForestClassifier(rnd_params)
+
+                rnd_clf.fit(X_train, y_train)
+
+                # 평가
+                train_y_pred = rnd_clf.predict(X_test)
+                train_acc = accuracy_score(y_test, train_y_pred)
+                test_y_pred = rnd_clf.predict(X_test)
+                test_acc = accuracy_score(y_test, test_y_pred)
+
+                print("random forest train acc : {:.4f}, test acc :{:.4f}".format(train_acc, test_acc))
+
+                # 히트맵으로 시각화하기
+                from sklearn.metrics import confusion_matrix
+
+                index = list(set(train.y))
+                rnd_clf_df = pd.DataFrame(confusion_matrix(y_test, test_y_pred), index=index, columns=index)
+
+                rnd_clf_df.to_csv('confusion_matrix.csv', index=False)
+                df_json = rnd_clf_df.to_json()
+
+
+
+            elif checked_list[1] == 'FNN':
+                pass
+            elif checked_list[1] == 'user_defined_machine_learning':
+                pass
+
+            # 세 번째 순서: 차원축소
+            dimension_reduction_train_data = dimension_reduction(checked_list[2], machine_learning_train_data)
+            dimension_reduction_test_data = dimension_reduction(checked_list[2], machine_learning_test_data)
 
         return render_template("index.html")
 
@@ -63,5 +138,14 @@ def main():
 def file_upload():
     return render_template("upload.html")
 
+@app.route("/confusion_matrix")
+def matrix():
+    df = pd.read_csv('heatmap_data.csv')
+    return df.to_csv()
+
+@app.route('/confusion_matrix/heatmap')
+def heatmap():
+    return render_template('visualization.html')
+
 if __name__ == "__main__":
-    app.run(host='localhost', port=8000, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
--- experience list/Daumsoft Internship/DataVisualization/embedding.py
+++ experience list/Daumsoft Internship/DataVisualization/embedding.py
@@ -1,11 +1,160 @@
-def embedding(method, data):
-    if method == "BoW":
-        pass
+import pandas as pd
+from scipy.sparse import csr_matrix
+
+def embedding(method, train, test):
+    # 데이터 섞어주기
+    # x열은 토크나이징 된 단어들 목록
+    # y열은 타겟 라벨
+    train = train.sample(frac=1).reset_index(drop=True)
+    test = test.sample(frac=1).reset_index(drop=True)
+
+    if method == "One-Hot":
+
+        # 데이터 준비
+        X_train_corpus = []
+        X_train_corpus2 = []
+        for words in train['x']:
+            sentence = []
+            for word in words.split(","):
+                sentence.append(word)
+
+            X_train_corpus.append(sentence)
+            X_train_corpus2.extend(sentence)
+
+        X_test_corpus = []
+        X_test_corpus2 = []
+        for words in test['x']:
+            sentence = []
+            for word in words.split(","):
+                sentence.append(word)
+
+            X_test_corpus.append(sentence)
+            X_test_corpus2.extend(sentence)
+
+        # 정수 인코딩
+        from collections import Counter
+
+        X_train_counts = Counter(X_train_corpus2)
+        X_test_counts = Counter(X_test_corpus2)
+
+        X_train_counts_sorted = sorted(X_train_counts.items(), key=lambda x: x[1], reverse=True)
+        X_test_counts_sorted = sorted(X_test_counts.items(), key=lambda x: x[1], reverse=True)
+
+        X_train_vocab = sorted(X_train_counts, key=X_train_counts.get, reverse=True)
+        X_test_vocab = sorted(X_test_counts, key=X_test_counts.get, reverse=True)
+
+        # 높은 빈도수를 가진 단어일수록 낮은 정수 인덱스 부여(훈련 데이터)
+        word_mapping_table = {}
+
+        idx = 0
+        for (word, frequency) in X_train_counts_sorted:
+            word_mapping_table[word] = idx
+            idx += 1
+
+        word_mapping_table_sorted = sorted(word_mapping_table.items(), key=lambda x: x[1], reverse=False)
+
+        X_train_Integer_Encoding = []
+        for sentence in X_train_corpus:
+            w = []
+            for word in sentence:
+                w.append(word_mapping_table[word])
+            X_train_Integer_Encoding.append(w)
+
+        X_test_Integer_Encoding = []
+        for sentence in X_test_corpus:
+            w = []
+            for word in sentence:
+                if word in word_mapping_table:
+                    w.append(word_mapping_table[word])
+                else:
+                    pass
+
+            X_test_Integer_Encoding.append(w)
+
+        # 원-핫 인코딩
+        X_train_Onehot_Encoding = list()
+        for value in X_train_Integer_Encoding:
+            letter = [0 for _ in range(len(word_mapping_table))]
+            for v in value:
+                letter[v] = 1
+            X_train_Onehot_Encoding.append(letter)
+
+        X_test_Onehot_Encoding = list()
+        for value in X_test_Integer_Encoding:
+            letter = [0 for _ in range(len(word_mapping_table))]
+            for v in value:
+                letter[v] = 1
+            X_test_Onehot_Encoding.append(letter)
+
+        sorted_mapping_keys = [word_mapping_table_sorted[idx][0] for idx in range(len(word_mapping_table_sorted))]
+
+        X_train_df = pd.DataFrame(X_train_Onehot_Encoding, columns=sorted_mapping_keys)
+        X_test_df = pd.DataFrame(X_test_Onehot_Encoding, columns=sorted_mapping_keys)
+
+        y_train_df = pd.DataFrame(train['y'].values, columns=['my_target'])
+        y_test_df = pd.DataFrame(test['y'].values, columns=['my_target'])
+
+        train_df = pd.concat([X_train_df, y_train_df], axis=1)
+        test_df = pd.concat([X_test_df, y_test_df], axis=1)
+
+        onehot_X_train = train_df.iloc[:, 0:len(sorted_mapping_keys)].values
+        onehot_y_train = train_df.loc[:, 'my_target'].values
+
+        onehot_X_test = test_df.iloc[:, 0:len(sorted_mapping_keys)].values
+        onehot_y_test = test_df.loc[:, 'my_target'].values
+
+        # sparse data로 변환
+        from scipy.sparse import csr_matrix
+
+        onehot_X_train = csr_matrix(onehot_X_train)
+        onehot_X_test = csr_matrix(onehot_X_test)
+
+        return onehot_X_train, onehot_X_test, onehot_y_train, onehot_y_test
+
     elif method == "TF-IDF":
-        pass
+
+        train_corpus = []
+        for words in train["x"].values:
+            sum_sentence = ""
+            for word in words.split(","):
+                sum_sentence += " " + word
+
+            sum_sentence = sum_sentence.strip()
+            train_corpus.append(sum_sentence)
+
+        test_corpus = []
+        for words in test["x"].values:
+            sum_sentence = ""
+            for word in words.split(","):
+                sum_sentence += " " + word
+
+            sum_sentence = sum_sentence.strip()
+            test_corpus.append(sum_sentence)
+
+
+        from sklearn.feature_extraction.text import TfidfVectorizer
+
+        tfidf = TfidfVectorizer()
+        tfidf_vectors = tfidf.fit_transform(train_corpus)
+
+        vocabs = tfidf.get_feature_names()
+        vectors = tfidf_vectors.toarray()
+
+        tfidf_df = pd.DataFrame(vectors, columns=vocabs)
+        target_df = pd.DataFrame(train["y"].values, columns=["target"])
+        tfidf_df = pd.concat([tfidf_df, target_df], axis=1)
+
+
+
+        print(tfidf_df)
+
+        X = tfidf_df.iloc[:, 0:len(vocabs)].values
+        y = tfidf_df.loc[:, 'target'].values
+
+        print(X.shape)
+        print(y.shape)
+
     elif method == "Doc2Vec":
         pass
     elif method == "user_defined_embedding":
         pass
-
-    return None
