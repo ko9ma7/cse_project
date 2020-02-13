23509f2087188bab476008e419f9842086730196
JiMyungHwa <40465192+JiMyungHwa@users.noreply.github.com>
2020-02-13 20:59:06 +0900

    Add files via upload

--- experience list/Daumsoft Internship/DataVisualization/app.py
+++ experience list/Daumsoft Internship/DataVisualization/app.py
@@ -53,35 +53,92 @@ def page3():
             print(checked_list)
 
             parameters = []
+            # 5개 파라미터
             if checked_list[1] == 'Logistic':
                 checked_list[3] = checked_list[3].split(" ")
-                parameters.append([float(i) for i in checked_list[3]])
-
-                params = {
-                    "C": parameters[0],
-                    "random_state": [42],
-                }
-
-            elif checked_list[1] == 'SVM':
                 checked_list[4] = checked_list[4].split(" ")
-                parameters.append([float(i) for i in checked_list[4]])
-
-                params = {
-                    "alpha": parameters[0],
-                    "random_state": [42],
-                }
-
-            elif checked_list[1] == 'RandomForest':
                 checked_list[5] = checked_list[5].split(" ")
                 checked_list[6] = checked_list[6].split(" ")
+                checked_list[7] = checked_list[7].split(" ")
+
+                parameters.append([str(i) for i in checked_list[3]])
+                parameters.append([float(i) for i in checked_list[4]])
                 parameters.append([int(i) for i in checked_list[5]])
                 parameters.append([int(i) for i in checked_list[6]])
 
+                if checked_list[7][0] != 'None':
+                    parameters.append([float(i) for i in checked_list[7]])
+                else:
+                    parameters.append([None])
+
                 params = {
-                    "n_estimators": parameters[0],
-                    "max_depth": parameters[1],
-                    "random_state": [42],
+                    "penalty": parameters[0],
+                    "C": parameters[1],
+                    "random_state": parameters[2],
+                    "max_iter": parameters[3],
+                    "l1_ratio": parameters[4],
                 }
+
+                print(params)
+
+            # 8개 파라미터
+            elif checked_list[1] == 'SVM':
+                checked_list[3] = checked_list[3].split(" ")
+                checked_list[4] = checked_list[4].split(" ")
+                checked_list[5] = checked_list[5].split(" ")
+                checked_list[6] = checked_list[6].split(" ")
+                checked_list[7] = checked_list[7].split(" ")
+                checked_list[8] = checked_list[8].split(" ")
+                checked_list[9] = checked_list[9].split(" ")
+                checked_list[10] = checked_list[10].split(" ")
+
+                parameters.append([str(i) for i in checked_list[3]])
+                parameters.append([str(i) for i in checked_list[4]])
+                parameters.append([float(i) for i in checked_list[5]])
+                parameters.append([float(i) for i in checked_list[6]])
+                parameters.append([int(i) for i in checked_list[7]])
+                parameters.append([int(i) for i in checked_list[8]])
+                parameters.append([str(i) for i in checked_list[9]])
+                parameters.append([float(i) for i in checked_list[10]])
+
+                params = {
+                    "loss": parameters[0],
+                    "penalty": parameters[1],
+                    "alpha": parameters[2],
+                    "l1_ratio": parameters[3],
+                    "max_iter": parameters[4],
+                    "random_state": parameters[5],
+                    "learning_rate": parameters[6],
+                    "eta0": parameters[7],
+                }
+
+                print(params)
+
+            # 3개 파라미터
+            elif checked_list[1] == 'RandomForest':
+                checked_list[3] = checked_list[3].split(" ")
+                checked_list[4] = checked_list[4].split(" ")
+                checked_list[5] = checked_list[5].split(" ")
+
+                parameters.append([int(i) for i in checked_list[3]])
+
+                if checked_list[4][0] != 'None':
+                    parameters.append([int(i) for i in checked_list[4]])
+                else:
+                    parameters.append([None])
+
+                parameters.append([int(i) for i in checked_list[5]])
+
+                params = {
+                        "n_estimators": parameters[0],
+                        "max_depth": parameters[1],
+                        "random_state": parameters[2],
+                    }
+
+                print(params)
+
+            else:
+                pass
 
             train = pd.read_csv(UPLOAD_FOLDER + "train.csv")
             test = pd.read_csv(UPLOAD_FOLDER + "test.csv")
@@ -94,6 +151,7 @@ def page3():
 
             # 임베딩만 시각화할 경우
             if checked_list[1] == '':
+                print('임베딩만 시각화할 경우')
                 return render_template('visualization.html', visualization="embedding_and_visualization")
 
             # 임베딩 -> 머신러닝 -> 시각화할 경우
@@ -116,8 +174,6 @@ def page3():
                 test_df.to_csv(path + 'embedding_and_machinelearning_visualization_test.csv', index=False)
 
                 return render_template('visualization.html', visualization="embedding_and_machineLearning_visualization")
-
-            return render_template('visualization.html', visualization="embedding_and_machineLearning_visualization")
 
         return render_template('machineLearning.html')
     else:
@@ -185,12 +241,14 @@ def data2_2():
 # 차원 축소 값을 받는 라우터
 @app.route('/embedding_and_visualization_train')
 def data3_1():
+    print('훈련 csv 파일 생성 완료')
     df = pd.read_csv(path + 'embedding_and_visualization_train.csv')
     return df.to_csv()
 
 
 @app.route('/embedding_and_visualization_test')
 def data3_2():
+    print('테스트 csv 파일 생성 완료')
     df = pd.read_csv(path + 'embedding_and_visualization_test.csv')
     return df.to_csv()
 
